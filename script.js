6be5696b4d3dbb35d068f3e95543378c665e632f
Sazhjar Nogoibaev <nogoibaevssanjar@gmail.com>
2021-06-06 01:05:49 +0600

    adding files

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 3;
+
+console.log(a % b);
+ 
+let c = 2;
+let d = 10;
+
+console.log(c ** d);
+const string = "1024";
+
+console.log(1 + "2" + " белых медведей");
+
+let e = 5;  let f = 10; 
+console.log(c = +e + f--);
+
+const x = 15 * (4 + 25 - 55);
+console.log(x)
